6e7146d080763b10f1f5d3d1834fae37540bfb12
Hudson Alves <hudsonralves@hotmail.com>
2023-07-20 21:34:14 -0300

    :boom: Ajuste caminho pastas

--- src/main/java/com/example/mapstruct/MapstructApplication.java
+++ src/main/java/com/example/mapstruct/MapstructApplication.java
@@ -1,4 +1,4 @@
-package com.example.mapstruct.mapstruct;
+package com.example.mapstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
